f7e5e4c123a3bd3250560f8e5732e4b2a09af466
Nathik <nathikazd@gmail.com>
2023-02-28 06:21:08 -0600

    working

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,183 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:mezcreen/env.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'package:mezcreen/sampleData.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await checkForTestData();
   runApp(const MyApp());
 }
+
+Future<void> checkForTestData() async {
+  DataSnapshot dataSnapshot =
+      await FirebaseDatabase.instance.ref().child(rootNode).get();
+  if (dataSnapshot.value != null) return;
+  await FirebaseDatabase.instance.ref().child(rootNode).set(data);
+}
+
+FirebaseDatabase database = FirebaseDatabase.instance;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  static const String _title = "App for $rootNode";
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: _title,
+      home: RoomsPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class RoomsPage extends StatefulWidget {
+  const RoomsPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<RoomsPage> createState() => _RoomsPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _RoomsPageState extends State<RoomsPage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      appBar: AppBar(title: const Text("Rooms")),
+      body: DefaultTextStyle(
+        style: Theme.of(context).textTheme.displayMedium!,
+        textAlign: TextAlign.center,
+        child: FutureBuilder<DataSnapshot>(
+          future: FirebaseDatabase.instance
+              .ref()
+              .child(rootNode)
+              .get(), // a previously-obtained Future<String> or null
+          builder:
+              (BuildContext context, AsyncSnapshot<DataSnapshot> snapshot) {
+            List<Widget> children = <Widget>[];
+            if (snapshot.hasData) {
+              Map<dynamic, dynamic> rooms =
+                  snapshot.data!.value as Map<dynamic, dynamic>;
+              rooms.forEach((key, value) {
+                children.add(TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              DevicesPage(roomKey: key, roomValue: value),
+                        ),
+                      );
+                    },
+                    child: Text(value['name'])));
+              });
+            } else if (snapshot.hasError) {
+              children = <Widget>[
+                const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text('Error: ${snapshot.error}'),
+                ),
+              ];
+            } else {
+              children = const <Widget>[
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Awaiting result...'),
+                ),
+              ];
+            }
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: children,
+              ),
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class DevicesPage extends StatefulWidget {
+  String roomKey;
+  dynamic roomValue;
+  DevicesPage({super.key, required this.roomKey, required this.roomValue});
+
+  @override
+  State<DevicesPage> createState() => _DevicesPageState();
+}
+
+class _DevicesPageState extends State<DevicesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.roomValue['name'])),
+      body: DefaultTextStyle(
+        style: Theme.of(context).textTheme.displayMedium!,
+        textAlign: TextAlign.center,
+        child: FutureBuilder<DataSnapshot>(
+          future: FirebaseDatabase.instance
+              .ref()
+              .child("$rootNode/${widget.roomKey}/devices")
+              .get(), // a previously-obtained Future<String> or null
+          builder:
+              (BuildContext context, AsyncSnapshot<DataSnapshot> snapshot) {
+            List<Widget> children = <Widget>[];
+            if (snapshot.hasData) {
+              Map<dynamic, dynamic> rooms =
+                  snapshot.data!.value as Map<dynamic, dynamic>;
+              rooms.forEach((key, value) {
+                children.add(
+                    TextButton(onPressed: () {}, child: Text(value['name'])));
+              });
+            } else if (snapshot.hasError) {
+              children = <Widget>[
+                const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text('Error: ${snapshot.error}'),
+                ),
+              ];
+            } else {
+              children = const <Widget>[
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Awaiting result...'),
+                ),
+              ];
+            }
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: children,
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
---
+++ lib/sampleData.dart
@@ -0,0 +1,34 @@
+dynamic data = {
+  "kitchen": {
+    "devices": {
+      "curtain": {"name": "Curtain", "type": "curtain", "value": 0},
+      "light1": {"name": "Light", "type": "light", "value": 70}
+    },
+    "name": "Kitchen"
+  },
+  "livingroom": {
+    "devices": {
+      "curtain": {"name": "Curtain", "type": "curtain", "value": 0},
+      "door": {"name": "Door", "type": "door", "value": 0},
+      "light1": {"name": "Light", "type": "light", "value": 70}
+    },
+    "name": "Living Room"
+  },
+  "room1": {
+    "devices": {
+      "curtain": {"name": "Curtain", "type": "curtain", "value": 0},
+      "fan": {"name": "Fan", "type": "fan", "value": 4},
+      "light1": {"name": "Light 1", "type": "light", "value": 70},
+      "light2": {"name": "Light 2", "type": "light", "value": 50}
+    },
+    "name": "Room 1"
+  },
+  "room2": {
+    "devices": {
+      "ac": {"name": "Airconditioner", "type": "ac", "value": 22},
+      "curtain": {"name": "Curtain", "type": "curtain", "value": 0},
+      "light1": {"name": "Light", "type": "light", "value": 70}
+    },
+    "name": "Room 2"
+  }
+};
